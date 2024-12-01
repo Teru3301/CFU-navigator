12eae68620609bfa0cc8ed3e3c27e358a2091eb1
Teru3301 <timurrumiancev@gmail.com>
2024-12-01 21:26:43 +0300

    добавлены кнопки переключения текущего отображаемого этажа

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -1,1 +1,45 @@
-alert('жава стрипт работаит')
+
+var current_floor = 0;
+var max_floor = 3;
+var min_floor = 0;
+
+
+function layer_up(map_image)
+{
+    if (current_floor < max_floor)
+    {
+        switch (current_floor)
+        {
+            case 0:
+                map_image.src = "./static/images/l1.png";
+                break;
+            case 1:
+                map_image.src = "./static/images/l2.png";
+                break;
+            case 2:
+                map_image.src = "./static/images/l3.png";
+                break;
+        }
+        current_floor++;
+    }
+}
+
+function layer_down (map_image)
+{
+    if (current_floor > min_floor)
+        {
+            switch (current_floor)
+            {
+                case 1:
+                    map_image.src = "./static/images/l0.png";
+                    break;
+                case 2:
+                    map_image.src = "./static/images/l1.png";
+                    break;
+                case 3:
+                    map_image.src = "./static/images/l2.png";
+                    break;
+            }
+            current_floor--;
+        }
+}
