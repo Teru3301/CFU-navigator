9e8ec0a2132493582ea99cf09c7ccc3f554ae597
Teru3301 <timurrumiancev@gmail.com>
2024-12-02 00:06:23 +0300

    Добавлен код загрузки данных графа из файла. Необходимо запускать локальный сервер

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -3,43 +3,47 @@ var current_floor = 0;
 var max_floor = 3;
 var min_floor = 0;
 
+var map_image = document.getElementById('map_image');
+var image_list = ["./static/images/l0.png", "./static/images/l1.png", "./static/images/l2.png", "./static/images/l3.png"];
 
-function layer_up(map_image)
+
+let lines;
+async function load_file() {
+    try {
+        const response = await fetch('./static/scripts/list.txt'); // Ожидаем завершения запроса
+        if (!response.ok) {
+            throw new Error(`Ошибка: ${response.status}`); // Проверяем успешность запроса
+        }
+        const text = await response.text(); // Ожидаем чтения текста
+        lines = text.split('\n'); // Разделяем текст по строкам
+    } catch (error) {
+        console.error('Произошла ошибка:', error); // Обрабатываем ошибки
+    }
+}
+(async () => {
+    await load_file();
+    for (var i = 0; i < lines.length; i++)
+        console.log(lines[i]);
+})();
+
+
+function layer_up()
 {
     if (current_floor < max_floor)
     {
-        switch (current_floor)
-        {
-            case 0:
-                map_image.src = "./static/images/l1.png";
-                break;
-            case 1:
-                map_image.src = "./static/images/l2.png";
-                break;
-            case 2:
-                map_image.src = "./static/images/l3.png";
-                break;
-        }
         current_floor++;
+        map_image.src = image_list[current_floor];
     }
 }
 
-function layer_down (map_image)
+function layer_down ()
 {
     if (current_floor > min_floor)
-        {
-            switch (current_floor)
-            {
-                case 1:
-                    map_image.src = "./static/images/l0.png";
-                    break;
-                case 2:
-                    map_image.src = "./static/images/l1.png";
-                    break;
-                case 3:
-                    map_image.src = "./static/images/l2.png";
-                    break;
-            }
-            current_floor--;
-        }
+    {
+        current_floor--;
+        map_image.src = image_list[current_floor];
+    }
 }
+
+
+
