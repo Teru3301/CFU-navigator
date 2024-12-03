fc4f112d7b52cf4e52beb3c742351a806a0910ee
Teru3301 <timurrumiancev@gmail.com>
2024-12-03 15:32:25 +0300

    данные из файла теперь записываются в граф

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -6,7 +6,7 @@ let min_floor = 0;
 let map_image = document.getElementById('map_image');
 let image_list = ["./static/images/l0.png", "./static/images/l1.png", "./static/images/l2.png", "./static/images/l3.png"];
 
-let graph;
+let graph = [];
 
 
 //          ЗАГРУЗКА ГРАФА ИЗ ФАЙЛА
@@ -25,16 +25,25 @@ async function load_file() {
     }
 }
 
+class connect
+{
+    constructor(name, distance)
+    {
+        this.name = name;
+        this.distance = distance;
+    }
+}
+
 class Node
 {
-    constructor(name, body, floor, x, y, connects)
+    constructor(name, body, floor, x, y, connections)
     {
         this.name = name;           //  название ноды (номер кабинета, туалет, мед-кабинет и т.д.)
         this.body = body;           //  корпус
         this.foor = floor;          //  этаж текущего узла (будет использоваться для отрисовки линий маршрута)
         this.x = x;                 //  x и y позиции текущего узла на карте
         this.y = y;
-        this.connects = connects;   //  список нод с которыми связана текущая и расстояние до них [{нода, расстояние}]  
+        this.connect = connections;   //  список нод с которыми связана текущая и расстояние до них [{нода, расстояние}]  
         this.distance = 0;          //  дистанция до этого узла от начального
         this.route = [this.name];   //  путь от начального узла до текущего
     }
@@ -46,9 +55,17 @@ class Node
     {
         let line = text[i].split(/\s+/);                    //  разделение строки на отдельные значения
         line = line.filter(item => item.trim() !== "");     //  удаление пустых элементов
+        if (line[0] == undefined) continue;                 //  пропуск пустых строк
 
-        for (let j = 0; j < line.length; j++)
-            console.log( "i = " + i + " j = " + j + " value = " + line[j]);
+        let connect_list = [];
+        for (let j = 0; j < line[5]; j++)
+        {
+            let connection = new connect(line[5 + j * 2], line[5 + j * 2 + 1]);
+            connect_list.push(connection);
+        }
+       let node = new Node(line[0], line[1], line[2], line[3], line[4], connect_list);
+
+        graph.push(node);
     }
 })();
 
